1ace9d1dfbd8981fc1b883716882565e3244eebc
Joao Ribeiro <jribeiro@digitalsales.pt>
2023-10-14 14:55:41 +0100

    005

---
+++ app/components/Footer.jsx
@@ -0,0 +1,13 @@
+export default function Footer() {
+    return (
+        <>
+            <footer className="container-fluid bg-black text-white mt-auto">
+                <div className="row">
+                    <div className="col p-3 text-center">
+                        conteúdo do footer aqui
+                    </div>
+                </div>
+            </footer>
+        </>
+    )
+}
--- app/layout.js
+++ app/layout.js
@@ -3,13 +3,15 @@ import "./globals.css"
 
 // components
 import Navbar from "./components/Navbar"
+import Footer from "./components/Footer"
 
 export default function RootLayout({ children }) {
   return (
     <html lang="pt">
-      <body>
+      <body class="d-flex flex-column min-vh-100">
         <Navbar />
         {children}
+        <Footer />
       </body>
     </html>
   )
