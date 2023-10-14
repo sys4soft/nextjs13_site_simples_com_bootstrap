4f7c1fdf8b32e4199fe69d1841c4f3e0327daaf8
Joao Ribeiro <jribeiro@digitalsales.pt>
2023-10-14 16:08:56 +0100

    008

--- app/map/page.jsx
+++ app/map/page.jsx
@@ -1,8 +1,14 @@
 export default function Map() {
     return (
         <>
-            <h1>Map</h1>
-            <p>Map here</p>
+            <iframe className="map"
+                width="100%"
+                height="100%"
+                frameborder="0"
+                marginheight="0"
+                marginwidth="0"
+                src="https://maps.google.com/maps?width=600&amp;height=400&amp;hl=en&amp;q=New York, NY, USA&amp;t=k&amp;z=11&amp;ie=UTF8&amp;iwloc=B&amp;output=embed">
+            </iframe>
         </>
     )
 }
